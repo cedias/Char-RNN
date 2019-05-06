4fe9b4661b299d3330dfe219a2fd8229d9eae1ef
Charles-Emmanuel Dias <Charles-Emmanuel.Dias@etu.upmc.fr>
2019-05-06 17:17:09 +0200

    fine

--- crnn.py
+++ crnn.py
@@ -22,10 +22,41 @@ all_characters = string.printable
 n_characters = len(all_characters)
 
 
+### Copied from AllenNLP library ==> https://allennlp.org/
+class InputVariationalDropout(torch.nn.Dropout):
+    """
+    Apply the dropout technique in Gal and Ghahramani, "Dropout as a Bayesian Approximation:
+    Representing Model Uncertainty in Deep Learning" (https://arxiv.org/abs/1506.02142) to a
+    3D tensor.
+    This module accepts a 3D tensor of shape ``(batch_size, num_timesteps, embedding_dim)``
+    and samples a single dropout mask of shape ``(batch_size, embedding_dim)`` and applies
+    it to every time step.
+    """
+    def forward(self, input_tensor):
+        # pylint: disable=arguments-differ
+        """
+        Apply dropout to input tensor.
+        Parameters
+        ----------
+        input_tensor: ``torch.FloatTensor``
+            A tensor of shape ``(batch_size, num_timesteps, embedding_dim)``
+        Returns
+        -------
+        output: ``torch.FloatTensor``
+            A tensor of shape ``(batch_size, num_timesteps, embedding_dim)`` with dropout applied.
+        """
+        ones = input_tensor.data.new_ones(input_tensor.shape[0], input_tensor.shape[-1])
+        dropout_mask = torch.nn.functional.dropout(ones, self.p, self.training, inplace=False)
+        if self.inplace:
+            input_tensor *= dropout_mask.unsqueeze(1)
+            return None
+        else:
+            return dropout_mask.unsqueeze(1) * input_tensor
+
 
 class RNN(nn.Module):
     
-    def __init__(self,  hidden_size, n_layers=1,rnn_cell=nn.RNN):
+    def __init__(self,  hidden_size, n_layers=1,dropout=0.5,rnn_cell=nn.RNN):
         """
         Create the network
         """
@@ -36,12 +67,15 @@ class RNN(nn.Module):
         self.hidden_size = hidden_size
         self.n_layers = n_layers
         
+        self.dropout = InputVariationalDropout(dropout,inplace=False)
+
         #  (batch,chunk_len) -> (batch, chunk_len, hidden_size)  
         self.embed = nn.Embedding(n_char,hidden_size)
         
         # (batch, chunk_len, hidden_size)  -> (batch, chunk_len, hidden_size)  
-        self.rnn = rnn_cell(hidden_size,hidden_size,batch_first=True)
+        self.rnns = nn.ModuleList([rnn_cell(hidden_size,hidden_size,batch_first=True) for _ in  range(n_layers)])
         
+
         #(batch, chunk_len, hidden_size) -> (batch, chunk_len, output_size)  
         self.predict = nn.Linear(hidden_size,n_char)
     
@@ -49,19 +83,31 @@ class RNN(nn.Module):
         """
         batched forward: input is (batch > 1,chunk_len)
         """
-        input = self.embed(input)
-        output,_  = self.rnn(input)
-        output = self.predict(output)
+        output = self.embed(input)
+       
+        for rnn in self.rnns:
+            do = self.dropout(output)
+            output,_ = rnn(do)
+        
+        output = self.predict(self.dropout(output))
         return output
     
-    def forward_seq(self, input,hidden=None):
+    def forward_seq(self, input,hiddens=None):
         """
         not batched forward: input is  (1,chunk_len)
         """
-        input = self.embed(input)
-        output,hidden  = self.rnn(input.unsqueeze(0),hidden)
+        output = self.embed(input)
+        output = output.unsqueeze(0)
+        new_hiddens = []
+        
+        if hiddens is None:
+            hiddens = [None for _ in self.rnns]
+
+        for rnn,hidden in zip(self.rnns,hiddens):
+            output,hidden  = rnn(output,hidden)
+            new_hiddens.append(hidden)
         output = self.predict(output)
-        return output,hidden
+        return output,new_hiddens
       
     
     
@@ -69,8 +115,8 @@ class RNN(nn.Module):
 class CharRNN(): 
 
   
-    def __init__(self,filename,load=None,device='cpu', hidden_size=350, n_layers=1,rnn_cell=nn.LSTM):
-        self.model = RNN( hidden_size, n_layers=1,rnn_cell=nn.RNN)
+    def __init__(self,filename,load=None,device='cpu', hidden_size=512, n_layers=3,rnn_cell=nn.LSTM):
+        self.model = RNN( hidden_size, n_layers=n_layers,rnn_cell=rnn_cell)
         self.device = device
         self.model.to(device)
 
@@ -93,7 +139,7 @@ class CharRNN():
 
     #### GENERATION #####
 
-    def generate(self,prime_str='A', predict_len=100, temperature=0.8):
+    def generate(self,prime_str='.', predict_len=100, temperature=0.8):
         prime_input = self.char_tensor(prime_str).squeeze(0)
         hidden = None
         predicted = prime_str+""
@@ -149,6 +195,7 @@ class CharRNN():
         Train sequence for one chunk:
         """
         #reset gradients
+        self.model.train()
         self.model_optimizer.zero_grad() 
         
         # predict output
@@ -160,11 +207,12 @@ class CharRNN():
         #compute gradients and backpropagate
         loss.backward() 
         self.model_optimizer.step() 
-
+        self.model.eval()
         return loss.data.item() 
 
 
     def train(self,iterations=1,chunk_len=110,batch_size=16, print_each=100):
+        
         self.model_optimizer= torch.optim.Adam(self.model.parameters())
         train_file = self.training_set_tensor(chunk_len) 
         data = DataLoader(train_file, batch_size=batch_size,shuffle=True)
@@ -182,7 +230,7 @@ class CharRNN():
                         print("-"*25)
                         print(f"Generated text at iter {iters}")
                         print("-"*25)
-                        print(self.generate(temperature=0.9))
+                        print(self.generate(temperature=0.8))
                         print("-"*25)
                         print(f"model-checkpointed in {self.checkpoint_file}")
                         print("")
@@ -197,10 +245,10 @@ class CharRNN():
                         break
 
 if __name__ == "__main__":
-    crnn = CharRNN("input.txt")
-    print(crnn.training_set_tensor(100))
+    crnn = CharRNN("input.txt",device="cuda:0",rnn_cell=nn.LSTM)
+    #print(crnn.training_set_tensor(100))
     crnn.load("charnn.chkpt")
-    #for chklen in range(500,1500):
-    crnn.train(100) # train for X epochs
-    print(crnn.generate())
-    crnn.save("model_350")
+    #for chklen in range(64,100):
+    crnn.train(200,batch_size=256,chunk_len=64) # train for X epochs
+    #print(crnn.generate())
+    crnn.save("ss_512")
